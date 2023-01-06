3c89a532e82046aa32b1d3406a839c898a500bf1
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2023-01-06 18:19:31 +0700

    [AC-47] register ColorScreen and CounterScreen in the app

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,6 +17,14 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Image Demo"
         onPress={() => navigation.navigate('Image')}
       />
+      <Button
+        title="Go to Counter Demo"
+        onPress={() => navigation.navigate('Counter')}
+      />
+      <Button
+        title="Go to Color Demo"
+        onPress={() => navigation.navigate('Color')}
+      />
     </View>
   );
 };
