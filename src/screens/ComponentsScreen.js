f61c2d729bb6d74e01c0535fbb1b729f7b763fab
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2023-01-06 17:44:51 +0700

    [AC-17] string interpolation in js

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,24 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the components screen</Text>;
+  const name = 'Stephen';
+
+  return (
+    <View>
+      <Text style={styles.textStyle}>Getting started with React Native!</Text>
+      <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30,
+    fontSize: 45
   },
+  subHeaderStyle: {
+    fontSize: 20
+  }
 });
 
 export default ComponentsScreen;
