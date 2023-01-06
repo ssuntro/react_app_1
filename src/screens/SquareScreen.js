6097a2055ddefacbf24c84c9c9262178a06dd2e4
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2023-01-06 18:40:52 +0700

    [AC-52] refactor

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,22 +2,37 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
-  console.log(red);
-
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
         color="Red"
       />
-      <ColorCounter color="Blue" />
-      <ColorCounter color="Green" />
+      <ColorCounter
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        color="Blue"
+      />
+      <ColorCounter
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        color="Green"
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`,
+        }}
+      />
     </View>
   );
 };
